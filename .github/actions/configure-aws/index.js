59089725b8ee85ec80cb008d880434fe51eb477e
Zachery Harley <zacheryharley@live.co.uk>
2023-06-26 21:57:09 +0100

    Added test code to commit and push

--- .github/actions/configure-aws/index.js
+++ .github/actions/configure-aws/index.js
@@ -1,7 +1,26 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: 'eu-west-2'})
 
 try {
+    var codeArtifact = new AWS.CodeArtifact({apiVersion: '2018-09-22'});
+    codeArtifact.listPackages({
+        domain: 'zacheryharley-java',
+        repository: 'lambda-light',
+        namespace: 'uk.co.zacheryharley',
+    }, function (err, data) {
+        data.packages.forEach(function (value) {
+            codeArtifact.listPackageVersions({}, function (e, d) {
+                d.versions.forEach(version => {
+                    console.log(d.package + ":" + version.version + ":" + version.status);
+                })
+            })
+        })
+    });
+
+
     const name = core.getInput('name');
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
